533d3bbc5d59081e53e74546f0895131e4bcf0cd
Md. Istiyak Ahmed <shuvrothpol@gmail.com>
2016-11-14 13:05:23 +0600

    change navbar size and color

--- ApacheLog/ApacheLog/settings.py
+++ ApacheLog/ApacheLog/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'logs',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
