fb139cb7065fb0e4609051e8815318c17190c355
Md. Istiyak Ahmed <shuvrothpol@gmail.com>
2016-11-14 15:48:21 +0600

    set default log format from list

--- ApacheLog/log_formats/models.py
+++ ApacheLog/log_formats/models.py
@@ -7,6 +7,7 @@ from sites.models import Site
 class LogFormats(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     log_format = models.CharField(max_length=100)
+    is_default = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.log_format
--- ApacheLog/log_formats/views.py
+++ ApacheLog/log_formats/views.py
@@ -49,6 +49,8 @@ def logformat_list_page(request):
 
 def logformat_edit_page(request, id=None):
     detail = get_object_or_404(LogFormats, id=id)
+    #import pdb;pdb.set_trace();
+
     form = LogFormatForm(request.POST or None, instance=detail)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -65,3 +67,14 @@ def logformat_delete_page(request, id=None):
     detail = get_object_or_404(LogFormats, id=id)
     detail.delete()
     return HttpResponseRedirect("/logformats/list/")
+
+def set_default_logformat(request, id=None):
+    detail = get_object_or_404(LogFormats, id=id)
+    site_id = detail.site_id
+
+    LogFormats.objects.filter(site_id=site_id).update(is_default=False)
+
+    LogFormats.objects.filter(id=detail.id).update(is_default=True)
+
+    return HttpResponseRedirect("/logformats/list/")
+
